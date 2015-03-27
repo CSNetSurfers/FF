bf9328e24d7118206ed9e0bb1183687d1d436426
yashodaa <yashodara13@gmail.com>
2015-03-27 09:42:53 -0700

    Third_part

--- truthtable.pl
+++ truthtable.pl
@@ -25,6 +25,21 @@ identify_variables(not X,Vin,Vout) :-   identify_variables(X,Vin,Vout).
 
 
 
+/*Generates the initial truth assignment*/
+initial_assign([],[]).
+initial_assign([X|T],[0|NewList]) :- initial_assign(T,NewList).
+
+/*Generates the next truth assignment*/
+successor(A,NewList) :- reverse(A,T),next(T,N),reverse(N,NewList).
+
+next([0|T],[1|T]).
+next([1|T],[0|NewList]) :- next(T,NewList).
+
+
+
+
+
+
 
 
 
