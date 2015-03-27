24bb200f15c8e479decc90f1beb3b41f40fca153
lushaka <lushakanisansala@ymail.com>
2015-03-27 10:05:35 +0530

    Second_Part

--- truthtable.pl
+++ truthtable.pl
@@ -3,3 +3,31 @@
 :- op(1000,xfy,'and').
 :- op(1000,xfy,'or').
 :- op(900,fy,'not').  
+
+
+
+
+
+/* identifies the variables in the expression */
+
+identify_variables(N,V,V) :- member(N,[0,1]),!. 
+
+identify_variables(X,Vin,Vout) :- atom(X), (member(X,Vin) -> Vout = Vin ; Vout = [X|Vin]).
+
+identify_variables(X and Y,Vin,Vout) :- identify_variables(X,Vin,Vtemp),identify_variables(Y,Vtemp,Vout).
+
+identify_variables(X or Y,Vin,Vout) :-  identify_variables(X,Vin,Vtemp),identify_variables(Y,Vtemp,Vout).
+
+identify_variables(not X,Vin,Vout) :-   identify_variables(X,Vin,Vout).
+
+
+
+
+
+
+
+
+
+
+
+
